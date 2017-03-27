1ee9671ad3f6cecafa8ae85a716db8e7e21a3c7b
vermosen <vermosen.jm@free.fr>
2017-03-27 12:17:45 -0400

    added yield curve bootstrap example

--- yieldCurve/src/main.cpp
+++ yieldCurve/src/main.cpp
@@ -7,10 +7,20 @@
 //============================================================================
 
 #include <iostream>
-#include <ql/time/date.hpp>
-#include <ql/time/calendars/unitedStates.hpp>
+#include <ql/time/all.hpp>
+#include <ql/currencies/america.hpp>
+#include <ql/settings.hpp>
+#include <ql/termstructures/yield/ratehelpers.hpp>
+#include <ql/termstructures/yield/piecewiseyieldcurve.hpp>
 
 namespace ql = QuantLib;
+
+struct Datum
+{
+    ql::Integer 	n		;
+    ql::TimeUnit 	units	;
+    ql::Rate 		rate	;
+};
 
 int main()
 {
@@ -19,8 +29,80 @@ int main()
 	const ql::Natural settlementDays = 2;
 	const ql::Date today = ql::Date::todaysDate();
 
-	ql::Date settlement = calendar.advance(today,settlementDays,ql::Days);
+	ql::Settings::instance().evaluationDate() = today;
+	ql::Date settlement = calendar.advance(today, settlementDays, ql::Days);
 
-	std::cout << settlement << std::endl;
+	std::vector<Datum> depositData =
+	{
+		{ 1, ql::Months, 0.04581 },
+		{ 2, ql::Months, 0.04573 },
+		{ 3, ql::Months, 0.04557 },
+		{ 6, ql::Months, 0.04496 },
+		{ 9, ql::Months, 0.04490 }
+	};
+
+	std::vector<Datum> swapData =
+	{
+		{  1, ql::Years, 0.0454 },
+		{  5, ql::Years, 0.0499 },
+		{ 10, ql::Years, 0.0547 },
+		{ 20, ql::Years, 0.0589 },
+		{ 30, ql::Years, 0.0596 }
+	};
+
+	// create the rate helpers
+	std::vector<boost::shared_ptr<ql::RateHelper>> instruments;
+
+	for(auto & it : depositData)
+	{
+		instruments.push_back(
+			boost::shared_ptr<ql::RateHelper>(
+				new ql::DepositRateHelper(
+					it.rate,
+					it.n * it.units,
+					settlementDays, calendar,
+					ql::ModifiedFollowing, true,
+					ql::Actual360())));
+	}
+
+	boost::shared_ptr<ql::IborIndex> index(
+		new ql::IborIndex("US libor 3M",
+			 3 * ql::Months,
+			 settlementDays,
+			 ql::USDCurrency(),
+			 calendar,
+			 ql::ModifiedFollowing,
+			 false,
+			 ql::Actual360()));
+
+	index->addFixing(settlement, depositData[2].rate);
+
+	for(auto & it : swapData)
+	{
+		instruments.push_back(
+			boost::shared_ptr<ql::RateHelper>(
+				new ql::SwapRateHelper(
+					it.rate,
+					it.n * it.units,
+					calendar, ql::Semiannual,
+					ql::Unadjusted, ql::Thirty360(),
+					index)));
+	}
+
+	auto ts = boost::shared_ptr<ql::YieldTermStructure>(
+		new ql::PiecewiseYieldCurve<ql::ForwardRate, ql::Cubic>(
+			settlement, instruments, ql::Actual360()));
+
+	for(int i = 0; i < 10; i++)
+	{
+		ql::Date t = settlement + ql::Period(i, ql::Years);
+
+		std::cout
+			<< "On " << t
+			<< ", zero rate is "
+			<< ts->zeroRate(t, ql::Actual365Fixed(), ql::Continuous)
+			<<  std::endl;
+	}
+
 	return 0;
 }
